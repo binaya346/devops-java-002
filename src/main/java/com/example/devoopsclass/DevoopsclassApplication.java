80b76212c7bbe851bfa5c39ed421098645dab20e
binayarijal-qbit <binaya.rijal@qbits.ltd>
2026-06-15 07:53:15 +0545

    initial commit

---
+++ src/main/java/com/example/devoopsclass/DevoopsclassApplication.java
@@ -0,0 +1,13 @@
+package com.example.devoopsclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoopsclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoopsclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devoopsclass/DevoopsclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devoopsclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoopsclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
