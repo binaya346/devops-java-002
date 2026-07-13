dd9acdfbaa57af6186304adc393d3562440d5e0f
binayarijal-qbit <binaya.rijal@qbits.ltd>
2026-07-13 07:32:05 +0545

    adds deploy file

--- src/main/java/com/example/devoopsclass/HelloController.java
+++ src/main/java/com/example/devoopsclass/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "DevOps Lab: Java Backend is officially RUNNING!";
+        return "Hello from Spring Boot application!";
     }
 }
