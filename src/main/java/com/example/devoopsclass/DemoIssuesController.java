46a675625fb65ccfd62344a87d287950f31a72c4
binayarijal-qbit <binaya.rijal@qbits.ltd>
2026-08-25 07:23:21 +0545

    Adds issues

--- src/main/java/com/example/devoopsclass/DemoIssuesController.java
+++ src/main/java/com/example/devoopsclass/DemoIssuesController.java
@@ -29,7 +29,6 @@ public class DemoIssuesController {
         if (input == defaultRole) {
             System.out.println("User is admin");
         }
-        
 
         // 🔴 4. BUG: Guaranteed NullPointerException (NPE)
         String nullableValue = null;
